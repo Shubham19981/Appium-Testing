34987e68a8ad812a2fdabcfb07e4967635397181
Shubham Dhoundiyal <151645894+Shubham19981@users.noreply.github.com>
2025-01-24 15:04:08 +0530

    Update GlobalParams.java

--- src/main/java/com/qa/utils/GlobalParams.java
+++ src/main/java/com/qa/utils/GlobalParams.java
@@ -76,7 +76,7 @@ public class GlobalParams {
     public void initializeGlobalParams(){
         GlobalParams params = new GlobalParams();
         params.setPlatformName(System.getProperty("platformName", "Android"));
-        params.setUDID(System.getProperty("udid", "100.108.210.70:5555"));
+        params.setUDID(System.getProperty("udid", "100.108.210.70:6666"));
         params.setDeviceName(System.getProperty("deviceName", "Nokia"));
          params.setappiumURL("http://100.105.134.36:4723/");
         switch(params.getPlatformName()){
