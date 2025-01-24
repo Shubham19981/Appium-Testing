58c6a2bcd9770eaea083fb350ce8d49f2a40f041
Shubham Dhoundiyal <151645894+Shubham19981@users.noreply.github.com>
2025-01-24 12:04:07 +0530

    Update CapabilitiesManager.java

--- src/main/java/com/qa/utils/CapabilitiesManager.java
+++ src/main/java/com/qa/utils/CapabilitiesManager.java
@@ -19,8 +19,7 @@ public class CapabilitiesManager {
             caps.setCapability("platformName", params.getPlatformName());
             caps.setCapability("udid", params.getUDID());
             caps.setCapability("deviceName", params.getDeviceName());
-            caps.setCapability("appiumURL", params.getappiumURL());
-             caps.setCapability("noReset","true");
+            caps.setCapability("noReset","true");
             switch(params.getPlatformName()){
                 case "Android":
                     caps.setCapability("automationName", props.getProperty("androidAutomationName"));
@@ -28,7 +27,6 @@ public class CapabilitiesManager {
                     caps.setCapability("appActivity", props.getProperty("androidAppActivity"));
                     caps.setCapability("systemPort", params.getSystemPort());
                     caps.setCapability("chromeDriverPort", params.getChromeDriverPort());
-                     caps.setCapability("appiumURL", params.getappiumURL());
                     
                     //String androidAppUrl = getClass().getResource(props.getProperty("androidAppLocation")).getFile();
                     String androidAppUrl = System.getProperty("user.dir") + File.separator + "src" + File.separator + "test"
